e4f7d38d085715b8a7baa632605a68203d653216
Anna Kiniova <anna.kiniova@gmail.com>
2019-08-11 18:04:36 +0300

    feat: add send message component

---
+++ src/components/sendMessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SendMessage = () => {
+  return (
+    <div>
+      <input type='textarea' placeholder = 'enter your message'></input>
+      <button>Send</button>
+    </div>
+  )
+};
+
+export default SendMessage;
+
+
