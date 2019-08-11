103b2e3033eb1f4f6ab4c7fd38e5a1cd11be6625
Anna Kiniova <anna.kiniova@gmail.com>
2019-08-11 22:19:55 +0300

    feat: moved websocket to App, refused websocket npm

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,17 +5,51 @@ import './App.css';
 import SendMessage from '../sendMessage'
 import Header from '../header'
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {
+    isOnline : false,
+    messages : []
+  }
+  url = 'ws://st-chat.shas.tel';
+  webSocket = new WebSocket(this.url);
 
-    <div className="App">
-      {/* <header className="App-header">
-      </header> */}
-      < Header />
-      < RenderMessage />
-      < SendMessage />
-    </div>
-  );
+  componentDidMount() {
+    this.webSocket.onopen = () => {
+      this.setState({
+        isOnline: true
+      })
+      console.log('online')
+    }
+
+    this.webSocket.onmessage = evt => {
+      const message = JSON.parse(evt.data);
+      this.addMessage(message);
+    }
+
+    this.webSocket.onclose = () => {
+      console.log('offline');
+      this.setState({
+        isOnline: false
+      })
+    }
+  }
+
+  addMessage = message =>
+    this.setState(state => ({ messages: [...message] }))
+
+  render() {
+    return (
+
+      <div className="App">
+        {/* <header className="App-header">
+        </header> */}
+        < Header data={this.state.isOnline}/>
+        < SendMessage />
+        < RenderMessage data={this.state.messages}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/renderMessage.js
+++ src/components/renderMessage.js
@@ -1,48 +1,24 @@
 import React from 'react';
-// import ConnectionPort from './ws';
-import Websocket from 'react-websocket';
+import PropTypes from 'prop-types';
 
 class RenderMessage extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      messages: []
-    };
-  }
-  componentDidMount(){
-    //ConnectionPort();
-  }
-
-  renderMessage(data) {
-
-    let messages = JSON.parse(data);
-    this.setState({count: this.state.count,
-      messages: messages});
-      console.log(this.state.messages);
-  }
-
-
   render() {
     return(
-    <>
-      <Websocket url='ws://st-chat.shas.tel'
-        onMessage={this.renderMessage.bind(this)}>
-      </Websocket>
-
-      {this.state.messages.map((item, i) => {
-         console.log(item);
-          return (
-          <div key={i}>{item.message}
-          </div>)
-        }
-        )
-      }
-
-    </>
+      <div>
+        {this.props.data.map((item, i) => {
+            console.log(item);
+            return (
+            <div key={i}>{item.message}
+            </div>)
+          }
+        )}
+      </div>
     )
   }
+}
+
+RenderMessage.propTypes = {
+  data:  PropTypes.array
 }
 
 export default RenderMessage;
