36a15f60d1186bf0c3d1b8f3efafbfc39f2eb9a7
Anna Kiniova <anna.kiniova@gmail.com>
2019-08-11 18:18:38 +0300

    feat: add header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import RenderMessage from '../renderMessage'
 
+import RenderMessage from '../renderMessage'
 import './App.css';
 import SendMessage from '../sendMessage'
+import Header from '../header'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
     <div className="App">
       {/* <header className="App-header">
       </header> */}
-
+      < Header />
       < RenderMessage />
       < SendMessage />
     </div>
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <div className = 'header'>
+      <div>
+        Name
+      </div>
+      <div>Status</div>
+      <div>date</div>
+    </div>
+  )
+}
+
+export default Header;
