f2637da5b7ceae37e8ea8caed1c6aa12bffa361b
Anna Kiniova <anna.kiniova@gmail.com>
2019-08-11 23:48:40 +0300

    feat: add styles to sendMessage and header

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './header.css'
 
 class Header extends React.Component {
 
   render() {
     return(
       <div className = 'header'>
-        <div>
+        <div className = 'header-item'>
           Name
         </div>
-        <div>Status
+        <div className = 'header-item'>Status
           <span style={this.statusStyle}>
           { this.props.data ? 'online' : 'offline'}
           </span>
         </div>
-        <div>date { (new Date()).toString() }</div>
+        <div className = 'header-item'>date { (new Date()).toString() }</div>
       </div>
     )
   }
--- src/components/sendMessage.js
+++ src/components/sendMessage.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const SendMessage = () => {
   return (
-    <div>
-      <input type='textarea' placeholder = 'enter your message'></input>
-      <button>Send</button>
-    </div>
+    <div className="input-group mb-3">
+      <input type="text" className="form-control" placeholder="enter your message here" aria-describedby="button-addon2" />
+      <div className="input-group-append">
+        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Send</button>
+      </div>
+      </div>
   )
 };
+
+
+
+
 
 export default SendMessage;
 
